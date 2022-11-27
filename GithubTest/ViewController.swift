6797098a1cc7453f0074a7c04e603d0c2413334e
Ghassan <ghassan.alshajjar@outlook.com>
2022-11-27 07:58:07 +0300

    committed hello1

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        label.textColor = .red
+        label.text = "Hello1"
     }
 
 
